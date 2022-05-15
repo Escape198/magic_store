a0ef0ec5e5b4126b2fdb9a69112468fb94199dd4
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-15 14:10:39 +0300

    Update books.py

--- magic/src/schemas/books.py
+++ magic/src/schemas/books.py
@@ -1,10 +1,10 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-from src.database.models import Film
+from src.database.models import Book
 
 
-class FilmSchema(SQLAlchemyAutoSchema):
+class BookSchema(SQLAlchemyAutoSchema):
     class Meta:
-        model = Film
+        model = Book
         exclude = ['id']
         load_instance = True
