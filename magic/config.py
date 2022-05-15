e3a3a27df9ab2357da6f79243b5553a3d61cf6fb
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-15 13:26:28 +0300

    Update config.py

--- magic/config.py
+++ magic/config.py
@@ -4,6 +4,7 @@ BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "magic_store.sqlite3")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "flask_tutorial.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'after-all-this-time?-always'
+    SECRET_KEY = 'you-will-never-know'
+    # SQLALCHEMY_ECHO = True
